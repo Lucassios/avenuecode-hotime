1d82cb0cb78df3a8e5d0559f4cc3eb2f4627e0f5
Lucas Marques <cmarques.lucas@gmail.com>
2017-06-10 10:06:26 -0300

    initial project

---
+++ src/main/java/com/avenuecode/hotime/HotimeApplication.java
@@ -0,0 +1,16 @@
+package com.avenuecode.hotime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lucascmarques on 10/06/17.
+ */
+@SpringBootApplication
+public class HotimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotimeApplication.class, args);
+    }
+
+}

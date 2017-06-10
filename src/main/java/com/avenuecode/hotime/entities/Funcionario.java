97552dd712d6a222fdb080633974029b1a3956cc
silassansil <silas_sansil@hotmail.com>
2017-06-10 10:51:31 -0300

    Ajustes mapeamento Localidade e Funcionarios

---
+++ src/main/java/com/avenuecode/hotime/entities/Funcionario.java
@@ -0,0 +1,67 @@
+package com.avenuecode.hotime.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * @author Silas
+ * @since 10/06/2017
+ */
+@Entity
+@Table
+public class Funcionario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String telefone;
+
+    @Column
+    private String email;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

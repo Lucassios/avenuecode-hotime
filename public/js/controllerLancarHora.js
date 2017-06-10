6d77ee8ed517230721d7a69edaa1f26505810e23
NELSON-PC\nelson <nelson.trindade.neto@gmail.com>
2017-06-10 17:40:05 -0300

    Serviço frontend horas lançadas

--- public/js/controllerLancarHora.js
+++ public/js/controllerLancarHora.js
@@ -1,29 +1,5 @@
-angular.module("hotime",[]).controller('LancarHoraController',ControllerSettings);
-
-function ControllerSettings(){
-	this.userId = "1234";
-	this.userPosition = [
-	{
-		id:"1",
-		description:"Tester"
-	},
-	{
-		id:"2",
-		description:"Marketing"
-	},
-	{
-		id:"3",
-		description:"Software Engineering"
-	}];
+angular.module("hotime").controller('LancarHoraController',function($scope){
 	
-	this.workedNormalHours = 4;
-	this.workedExtraHours = 4;
 	
-	this.HoursData = {
-			date:'10/12/1987',
-			normalHours:8,
-			extrasHours:10
 				
-	}
-	
-}
+});

d09371455286548d99643dbc9ea9e6a777d1d11b
Daniela Mara <dani_mara@msn.com>
2017-06-10 12:49:19 -0300

    Cargo refatorado.

--- src/main/java/com/avenuecode/hotime/controller/CargoController.java
+++ src/main/java/com/avenuecode/hotime/controller/CargoController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,29 +17,29 @@ import com.avenuecode.hotime.entities.Cargo;
 import com.avenuecode.hotime.repository.CargoRepository;
 
 @RestController
+@RequestMapping(value = "/cargo")
 public class CargoController {
 	
     @Autowired
     private CargoRepository cargoRepository;
 
-    @RequestMapping(value = "/cargo", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Cargo criarCargo(@RequestBody Cargo cargo) {
         return cargoRepository.save(cargo);
     }
 
-    @RequestMapping(value = "/cargo/{cargoId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/{cargoId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public void removerCargo(@PathVariable Long cargoId) {
         cargoRepository.delete(cargoId);
     }
 
-    @RequestMapping(value = "/cargo/{cargoId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Cargo buscar(@PathVariable  Long cargoId) {
+    @GetMapping(value = "/{cargoId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Cargo buscarCargo(@PathVariable  Long cargoId) {
         return cargoRepository.findOne(cargoId);
     }
 
-    @RequestMapping(value = "/cargos", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Cargo> buscarTodas() {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cargo> buscarTodos() {
         return (List) cargoRepository.findAll();
     }
-
 }

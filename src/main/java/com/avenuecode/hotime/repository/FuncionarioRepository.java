52a3133e7a098250679f49532bf769eef5ea9d93
silassansil <silas_sansil@hotmail.com>
2017-06-10 11:56:29 -0300

    Funcionario Controller e repository OK

---
+++ src/main/java/com/avenuecode/hotime/repository/FuncionarioRepository.java
@@ -0,0 +1,13 @@
+package com.avenuecode.hotime.repository;
+
+import com.avenuecode.hotime.entities.Funcionario;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Silas
+ * @since 10/06/2017
+ */
+@Repository
+public interface FuncionarioRepository extends CrudRepository<Funcionario, Long> {
+}

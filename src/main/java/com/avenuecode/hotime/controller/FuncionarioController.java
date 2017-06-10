f33c9c1d6f6b3bb095b7160dc45f55a174468361
silassansil <silas_sansil@hotmail.com>
2017-06-10 12:38:31 -0300

    Valor hora Controller e repository OK

--- src/main/java/com/avenuecode/hotime/controller/FuncionarioController.java
+++ src/main/java/com/avenuecode/hotime/controller/FuncionarioController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -16,28 +15,29 @@ import java.util.List;
  * @since 10/06/2017
  */
 @RestController
-public class FuncionarioController implements Serializable {
+@RequestMapping(value = "/funcionario")
+public class FuncionarioController {
 
     @Autowired
     private FuncionarioRepository funcionarioRepository;
 
 
-    @RequestMapping(value = "/funcionario", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public Funcionario criarFuncionario(@RequestBody Funcionario funcionario) {
         return funcionarioRepository.save(funcionario);
     }
 
-    @RequestMapping(value = "/funcionario/{funcionarioId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/{funcionarioId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
     public void removerfuncionario(@PathVariable Long funcionarioId) {
         funcionarioRepository.delete(funcionarioId);
     }
 
-    @RequestMapping(value = "/funcionario/{funcionarioId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/{funcionarioId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Funcionario buscar(@PathVariable Long funcionarioId) {
         return funcionarioRepository.findOne(funcionarioId);
     }
 
-    @RequestMapping(value = "/funcionarios", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Funcionario> buscarTodas() {
         return (List) funcionarioRepository.findAll();
     }

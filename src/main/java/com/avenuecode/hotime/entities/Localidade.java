c5c37ee3577df6e43c30810e204a6adf54bbfb89
silassansil <silas_sansil@hotmail.com>
2017-06-10 10:48:53 -0300

    Ajustes mapeamento Localidade

--- src/main/java/com/avenuecode/hotime/entities/Localidade.java
+++ src/main/java/com/avenuecode/hotime/entities/Localidade.java
@@ -1,17 +1,53 @@
 package com.avenuecode.hotime.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by lucascmarques on 10/06/17.
  */
 @Entity
+@Table
 public class Localidade {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @Column
+    private String cidade;
+
+    @Column
+    private String pais;
+
+    public Localidade() {
+    }
+
+    public Localidade(String cidade, String pais) {
+        this.cidade = cidade;
+        this.pais = pais;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
 }
